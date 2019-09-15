e47b35c66c571ed1f146f02c691b8f74f9833568
jeffcheung <jeffcheung2015@gmail.com>
2019-09-15 14:30:41 +0800

    database settings updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,22 +31,22 @@ GOOGLE_RECAPTCHA_SECRET_KEY = '6LddeLgUAAAAAETSEErsr7dG9lJhtxFBXmFPhf7U'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-dev.ap-southeast-1.elasticbeanstalk.com', 'localhost', '127.0.0.1', '172.31.37.220']
+ALLOWED_HOSTS = ['django-dev.ap-southeast-1.elasticbeanstalk.com', 'localhost', '127.0.0.1']
 
-import requests
+# import requests
 
 ########## ALLOWED_HOSTS
-from requests.exceptions import ConnectionError
-
-url = "http://169.254.169.254/latest/meta-data/public-ipv4"
-try:
-    r = requests.get(url)
-    instance_ip = r.text
-    ALLOWED_HOSTS += [instance_ip]
-except ConnectionError:
-    error_msg = "You can only run production settings on an AWS EC2 instance"
-    raise ImproperlyConfigured(error_msg)
-########## END ALLOWED_HOSTS
+# from requests.exceptions import ConnectionError
+#
+# url = "http://169.254.169.254/latest/meta-data/public-ipv4"
+# try:
+#     r = requests.get(url)
+#     instance_ip = r.text
+#     ALLOWED_HOSTS += [instance_ip]
+# except ConnectionError:
+#     error_msg = "You can only run production settings on an AWS EC2 instance"
+#     raise ImproperlyConfigured(error_msg)
+# ########## END ALLOWED_HOSTS
 
 # Application definition
 
@@ -115,23 +115,26 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ebdb',
-        'USER': 'djangodbadmin',
-        'PASSWORD': 'eQX13fcGPa5sfYE5dZXW',
-        'HOST': 'aakmdcrdqeaayo.carcvihijgzg.ap-southeast-1.rds.amazonaws.com',
-        'PORT': '5432',
+DATABASES = {}
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
